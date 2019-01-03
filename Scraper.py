3ed5416df10b83a6818c001e006f3fbb92b28528
Wesley Guergah <wguergah10556@g.ucla.edu>
2019-01-03 11:28:42 -0800

    Made a program that scraped the names of UC Berkeley's Math Graduate Students

--- Scraper.py
+++ Scraper.py
@@ -1,52 +1,24 @@
 # A method in object-oriented programming is a procedure associated with a class
 import requests, re, string, random
 # A scraper that scraps all links or images and saves them to a list. Organize indexed content and remove duplicates. Put in searchable file.
-def getRandomURLS(num):
-	randomURLS=[]
-	for z in range(0,num):
-		charList = []
-		for i in range(0,5):
-			char = chr(random.randint(97,122))
-			charList.append(char)
-		charList.append(".com")
-		charList[0] = 'https://www.'
-		randomURLS.append(''.join(charList))
-	return randomURLS
-
-arandomURL = getRandomURLS(1)[1]
-anotherrandomURL = getRandomURLS(1)[1]
 
 
-"""
-def getcontentType(randomURLS):
-	contentType = []
-	for i in range(0,len(randomURLS)):
-		try:
-			contentType.append(str("Wesbite Number " + str(i) + ": " + requests.get(randomURLS[i]).headers['Content-Type']))
-		except Exception, Warning:
-			contentType.append("Content Not Provided")
-	return contentType
+URL = "https://math.berkeley.edu/people/grad"
+patternRE = "/people/grad/.*\">"
+webTEXT = requests.get(URL,timeout=1).text
+linkList = re.findall(patternRE,webTEXT)
+patternNAME = "/people/grad/"
 
-	def getcontentType(randomURLS):
-	contentType = []
-	for i in range(0,len(randomURLS)):
-		try:
-			contentType.append(str("Wesbite Number " + str(i) + ": " + requests.get(randomURLS[i]).headers['Content-Type']))
-		except Exception, Warning:
-			contentType.append("Content Not Provided")
-	return contentType
-"""
+def getName(thing):
+	closer = thing.split(patternNAME)[1]
+	name = closer.split('">')
+	return name[0]
 
-def getcontentType(randomURLS):
-	contentType = []
-	for i in range(0,len(randomURLS)):
-		try:
-			contentType.append(requests.get(randomURLS[i], timeout=1).headers['Content-Type'])
-		except requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout:
-			contentType.append("Content not provided")
-	return contentType
+nameList = [None]*len(linkList)
+for i in range(0,len(linkList)):
+	nameList[i] = getName(linkList[i])
+nameList_final = [None]*len(linkList)
+for i in range(0,len(linkList)):
+	nameList_final[i]=str(nameList[i])
 
-
-
-print(getcontentType(getRandomURLS(11)))
 
