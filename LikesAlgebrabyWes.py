91bed192655a1b2ff2e7056006ae33aad880cfbe
Wesley Guergah <wguergah10556@g.ucla.edu>
2019-01-15 23:59:52 -0800

    blah

--- LikesAlgebrabyWes.py
+++ LikesAlgebrabyWes.py
@@ -8,7 +8,8 @@
 import requests, os.path, re
 
 body = re.compile("<body>.*</body>",re.S)
-algebra = re.compile("algebra")
+algebra = re.compile("algebra",re.IGNORECASE)
+urllist = ["http://www.math.ucla.edu/~mikel/","https://www.math.ucla.edu/~tao/","http://math.stanford.edu/~ksound/","https://math.berkeley.edu/people/grad/jed-duersch"]
 
 def getURL():
 	print("If you make a typo on the URL, just find out how to start the program or else it will fail...")
@@ -18,7 +19,7 @@ def istextand200(url):
 	return requests.get(url, timeout=5).status_code == 200 and requests.get(url, timeout=5).headers['content-type'].split('/')[0].lower() == 'text'
 def getText(url):
 	try:
-		return requests.get(url, timeout = 1).text.encode(encoding="utf-8")
+		return requests.get(url, timeout = 1).text
 	except requests.exceptions.RequestException:
 		print(" Professor hates providing his text. He uses cryptic methods or a lot of flair on his Papyrus. ")
 		print(" You'd better try someone more down to Earth.")
@@ -30,7 +31,7 @@ def hastheAlgebra(text):
 	return False
 
 
-def Main():
+def enteranySite():
 	URL = getURL()
 	if istextand200(URL):    		
 		siteText = getText(URL)
@@ -38,9 +39,17 @@ def Main():
 			print("This persons web page contained the word algebra in it. Cool.")
 		else:
 			print("This persons web page did not contain the word algebra in it.")
-		
 
-Main()
+def getPercentage(urllist):
+	count=0
+	count = float(count)
+	for url in urllist:
+		if(hastheAlgebra(getText(url))):
+			count+=1
+	return "{} percent of the sites had the word algebra!".format(count/float(len(urllist)))
+
+print(getPercentage(urllist))
+
 #print(istextand200("http://www.math.ucla.edu/~mikel/"))
 """
 print(hastheAlgebra(getText("http://math.stanford.edu/~ksound/")))
