3724cfcec3fae12577b1ca110e2a8ce4159eb75a
Wesley Guergah <wguergah10556@g.ucla.edu>
2019-01-15 23:16:01 -0800

    created a program that searched for the word Algebra on a few professors web pages

---
+++ LikesAlgebrabyWes.py
@@ -0,0 +1,52 @@
+## A Bad Python Program That Checks if Professor John Doe or His Minion Typed Algebra on His HTML Papyrus Scroll ##
+## Written by a feable plebian named G YELSEW that hates NASA ##
+## Because why explore outer space ##
+### With my research funds ##
+## When you don't even understand ##
+## Planet Earth ##
+
+import requests, os.path, re
+
+body = re.compile("<body>.*</body>",re.S)
+algebra = re.compile("algebra")
+
+def getURL():
+	print("If you make a typo on the URL, just find out how to start the program or else it will fail...")
+	URL = input("Enter a URL such as http://www.math.ucla.edu/~mikel/ or https://www.math.ucla.edu/~tao/ or whichever monk you'd like. ")
+	return URL   
+def istextand200(url):	 
+	return requests.get(url, timeout=5).status_code == 200 and requests.get(url, timeout=5).headers['content-type'].split('/')[0].lower() == 'text'
+def getText(url):
+	try:
+		return requests.get(url, timeout = 1).text.encode(encoding="utf-8")
+	except requests.exceptions.RequestException:
+		print(" Professor hates providing his text. He uses cryptic methods or a lot of flair on his Papyrus. ")
+		print(" You'd better try someone more down to Earth.")
+
+def hastheAlgebra(text):
+	match = algebra.search(text)
+	if match:
+		return True
+	return False
+
+
+def Main():
+	URL = getURL()
+	if istextand200(URL):    		
+		siteText = getText(URL)
+		if hastheAlgebra(siteText):
+			print("This persons web page contained the word algebra in it. Cool.")
+		else:
+			print("This persons web page did not contain the word algebra in it.")
+		
+
+Main()
+#print(istextand200("http://www.math.ucla.edu/~mikel/"))
+"""
+print(hastheAlgebra(getText("http://math.stanford.edu/~ksound/")))
+print(hastheAlgebra(getText("https://www.math.ucla.edu/~tao/")))
+print(hastheAlgebra(getText("https://math.berkeley.edu/people/grad/jed-duersch")))
+print(hastheAlgebra(getText("http://www.math.ucla.edu/~mikel/")))
+"""
+
+
